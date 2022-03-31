dc130667556ce690884e059b2414b44f8a013923
Rajesh Keshoju <rajeshkeshoju9@gmail.com>
2022-03-31 18:57:31 +0530

    Update temp.c

--- troubleshoot/temp.c
+++ troubleshoot/temp.c
@@ -1,12 +1,9 @@
-																																																#include <stdio.h>
+#include <stdio.h>
+
 int main() {
-   int i, j, rows=5;
-   for (i = 1; i <= rows; ++i) {
-      for (j = 1; j <= i; ++j) {
-         printf("* ");
-      }
-      printf("\n");
-   }
-   return 0;
+
+	char* tester = "Rajesh Keshoju";
+	printf("Troubleshoot test by %s", tester);
+
+	return 0;
 }
-																																																																																							
